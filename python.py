246830b51426efc5a1de12829fcfb59b10f3a717
Knud Petersen <knudpetersen@Knuds-MacBook-Pro.local>
2020-03-13 18:50:42 +0100

    heyhey

--- python.py
+++ python.py
@@ -4,3 +4,17 @@ for i in range(tal):
     print("Hello world!")
 
 print("doh")
+
+
+s = "abcdollibolliefghijk"
+
+current = s[1]
+longest = current
+
+for letter in s[1:]:
+    if letter >= current:
+        longest = current
+    else:
+        current += letter
+print(longest)
+
